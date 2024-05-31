fb32d083e1615a3f145f8e87ee4b487badae9221
Pedro Henrique <pedro2266809ph@gmail.com>
2024-05-31 15:09:43 -0300

    first commit

---
+++ src/main/java/io/github/pedrohss2/msclientes/MsclientesApplication.java
@@ -0,0 +1,14 @@
+package io.github.pedrohss2.msclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class MsclientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsclientesApplication.class, args);
+	}
+}
---
+++ src/test/java/io/github/pedrohss2/msclientes/MsclientesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.pedrohss2.msclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsclientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
